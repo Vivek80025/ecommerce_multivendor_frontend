8d40615a8920040b2671f7961e165d30a24b58dc
Vivek80025 <vjha052003@gmail.com>
2026-01-08 18:26:25 +0530

    change backend url4

--- src/Config/Api.ts
+++ src/Config/Api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // export const API_URL = "http://localhost:5454";
-export const API_URL = "https://qejhvsdtxfpx.ap-southeast-1.clawcloudrun.com";
+export const API_URL = "https://pxjzljhkkjka.ap-southeast-1.clawcloudrun.com";
 
 export const api = axios.create({
   baseURL: API_URL, 
